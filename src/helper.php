4276d6515dceb4c5dbf6796eb693a63413ee6b2d
Mehran NikNafs <mehran.niknafs@gmail.com>
2020-11-22 13:50:46 +0330

    Fix randomNumber function

--- src/helper.php
+++ src/helper.php
@@ -1,35 +1,33 @@
-<?php 
+<?php
 namespace Ybazli\Faker;
 
-
-// string helper	
+// string helper
 if (!function_exists('string')) {
     function string($value)
     {
-        return (string)$value;
+        return (string) $value;
     }
 }
 //get rand int
-if(! function_exists('randomNumber')){
+if (!function_exists('randomNumber')) {
     function randomNumber($length = 20, $int = false)
     {
         $numbers = "0123456789";
 
-        $number  = '';
+        $number = '';
 
-        for($i = 1;$i <= $length;$i++){
-            $num = $numbers[rand(0,strlen($numbers) - 1)];
-            
-            if($num == 0 && $i == 1){
-                $i = 1;
-                continue;
+        for ($i = 1; $i <= $length; $i++) {
+            if ($i == 1) {
+                $num = $numbers[rand(1, strlen($numbers) - 1)];
+            } else {
+                $num = $numbers[rand(0, strlen($numbers) - 1)];
             }
 
-            $number .= $num ;
+            $number .= $num;
         }
 
-        if($int){
-            return (integer)$number;
+        if ($int) {
+            return (integer) $number;
         }
 
         return string($number);
